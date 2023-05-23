4907dcb5b0ef98653b77275b8087de20d81e25c2
CSE-Helper <99971263+CSE-Helper@users.noreply.github.com>
2023-05-23 22:27:55 +0530

    Update 13.fileSystem.js

--- Codes/13.fileSystem.js
+++ Codes/13.fileSystem.js
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/style.css">
+  </head>
+  <body>
+    <h1 id="header"></h1>
+
+    <script src="src/script.js"></script>
+  </body>
+</html>
+
+//script.js
 const fs = require('fs');
 
 let data = "This is a file containing a collection of books.";
